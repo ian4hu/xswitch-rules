966938ed1d261f692e0b8836d7593cbb981cd6eb
ian4hu <hu2008yinxiang@163.com>
2022-02-07 11:18:12 +0800

    commit

---
+++ aes.js
@@ -0,0 +1,10 @@
+goldlog = {}
+goldlog.record = function() {}
+goldlog.setPageSPM = function() {}
+goldlog.sendPV = function() {}
+aplus_queue = []
+goldlog = new Proxy(goldlog, {
+  get: (target, p) => function(){},
+  set: (target, p, v) => console.log(p, v)
+})
+aplus = goldlog;
